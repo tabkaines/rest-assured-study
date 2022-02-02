6bf67dbe3305782fe9c5d228cbf1b85d3364b458
Ines Tabka <ines.tabka@cgi.com>
2022-02-02 14:23:21 -0500

    added VegetablesTest file

---
+++ src/test/java/comxyzcorp/VegetablesTest.java
@@ -0,0 +1,23 @@
+package comxyzcorp;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class VegetablesTest {
+    //[{"description":"Root vegetable, usually orange","name":"Carrot"},
+    // {"description":"Summer squash","name":"Zucchini"}]
+    @Test
+    public void testGetVegetables() {
+        given()
+                .relaxedHTTPSValidation()
+                .accept(ContentType.JSON)
+                .when()
+                .get("https://staging.tiered-planet.net/mild-temper/legumes")
+                .then()
+                .assertThat()
+                .body("[0].description", equalTo("Root vegetable, usually orange"));
+    }
+}
