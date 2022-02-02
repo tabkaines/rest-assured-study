0946b9841f0cd56b113d0551b4ada97f260de5e2
Ines Tabka <ines.tabka@cgi.com>
2022-02-02 11:56:22 -0500

    added FruitTest file

---
+++ src/test/java/comxyzcorp/FruitTest.java
@@ -0,0 +1,27 @@
+package comxyzcorp;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class FruitTest {
+    //Rest-Assured
+    //[{"description":"Winter fruit","name":"Apple"},
+    // {"description":"Tropical fruit","name":"Pineapple"}]
+    @Test
+    public void testGetFruits() {
+        given()
+                .relaxedHTTPSValidation()
+                .accept(ContentType.JSON)
+                .when()
+                .get("https://staging.tiered-planet.net/mild-temper/fruits")
+                .then()
+                .assertThat()
+                .body("[0].description", equalTo("Winter fruit"));
+    }
+
+
+}
